a9115d483efc2ab784b1ba3d8718e6f7fc0b6343
Kevin Provance <kevin.provance@gmail.com>
2021-07-01 15:47:17 -0400

    Update run.php

--- run.php
+++ run.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * getSubsets Function.
  * Clean up the Google Webfonts subsets to be human readable
@@ -92,32 +91,22 @@ function getVariants( $var ) {
 date_default_timezone_set( 'UTC' );
 
 $output = shell_exec( 'git log -1' );
-//echo $output . "\n\n";
-//if ( strpos( $output, 'Author: Travis CI' ) === false ) {  //WHy cron is failing, this line there
 echo shell_exec( 'git checkout -f master' );
-$gFile = dirname( __FILE__ ) . '/google_fonts.json';
-if ( file_exists( $gFile ) ) {
-	// Keep the fonts updated weekly
-	$weekback     = strtotime( date( 'jS F Y', time() + ( 60 * 60 * 24 * - 7 ) ) );
-	$last_updated = filemtime( $gFile );
+$gFile             = dirname( __FILE__ ) . '/google_fonts.json';
+$gFileminPHP       = dirname( __FILE__ ) . '/googlefonts.php';
 
-	if ( $last_updated >= $weekback ) {
-		//echo 'Exit update.  A week has not yet passed.';
-		//return;
-	}
-}
-
-$fonts = array();
+$fonts             = array();
 
 $arrContextOptions = array(
-	"ssl" => array(
-		"verify_peer"      => false,
-		"verify_peer_name" => false,
+	'ssl' => array(
+		'verify_peer'      => false,
+		'verify_peer_name' => false,
 	),
 );
 
-$key    = getenv( 'GOOGLEKEY' );
-$result = json_decode( file_get_contents( "https://www.googleapis.com/webfonts/v1/webfonts?key={$key}", false, stream_context_create( $arrContextOptions ) ) );
+$key               = $argv[1];
+$result            = json_decode( file_get_contents( "https://www.googleapis.com/webfonts/v1/webfonts?key={$key}", false, stream_context_create( $arrContextOptions ) ) );
+$cd                = date( 'Y-m-d h:i:s:a' );
 
 foreach ( $result->items as $font ) {
 	$fonts[ $font->family ] = array(
@@ -126,10 +115,17 @@ foreach ( $result->items as $font ) {
 	);
 }
 
-ksort($fonts);
-
+ksort( $fonts );
 $data = json_encode( $fonts );
 
+echo "Saving JSON File\n\n";
 file_put_contents( $gFile, $data );
 
-echo "Saved new JSON\n\n";
+echo "Saving PHP\n\n";
+$code = <<<PHP
+<?php
+// Last Updated : $cd
+defined( 'ABSPATH' ) || exit; 
+return json_decode( '$data', true );
+PHP;
+file_put_contents( $gFileminPHP, $code );
